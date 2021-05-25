c6f78f81c72db3c519beb942510d6d16a77a8ca5
dattamks <66517145+dattamks@users.noreply.github.com>
2021-05-25 12:25:16 +0530

    commit 5
    none

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e&f$&_)i6=2*%23^-zf$-)u8q$ny8)=e0_%%-bgalqf^m%($97'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
